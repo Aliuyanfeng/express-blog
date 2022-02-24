da83b72481db1cb36a5ad9f4b1845d0748afba14
Aliuyanfeng <1773189480@qq.com>
2022-02-24 10:10:22 +0800

    merge code

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 // 配置body-parser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
