466ad374bbfd622f272e6068108cea934012b2d7
Aliuyanfeng <1773189480@qq.com>
2022-03-17 10:29:59 +0800

    merge code

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,8 @@ const path = require('path')
 var storage = multer.diskStorage({
   //文件保存路径
   destination: function (req, file, cb) {
-    // cb(null, path.join(__dirname, "../uploads"))
-    cb(null,'./upload/images')
+    cb(null, path.join(__dirname, "/"))
+    // cb(null,'./upload/images')
   },
   //修改文件名称
   filename: function (req, file, cb) {
